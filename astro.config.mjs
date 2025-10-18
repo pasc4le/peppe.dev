c3c14225e9d1e080eb77aa91678db0bd63628de9
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2025-10-18 17:12:50 +0200

    refactor: running prettier on everything

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
-import svelte from '@astrojs/svelte';
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), icon(), svelte()],
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-  adapter: vercel()
+  adapter: vercel(),
 });
