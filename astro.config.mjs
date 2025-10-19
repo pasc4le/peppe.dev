2cd82867f46dfa56c975e0ebd3c8a40f52843327
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2025-10-19 17:34:07 +0200

    chore: updating output astro type

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   integrations: [mdx(), icon(), svelte(), db()],
   vite: {
     plugins: [tailwindcss()],
